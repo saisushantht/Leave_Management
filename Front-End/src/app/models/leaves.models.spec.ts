07151289f403197b38043c3f3826b68739dee82f
Thakkalapally Sai Sushanth <47238397+saisushantht@users.noreply.github.com>
2021-11-26 20:49:23 +0530

    Delete Front-End directory

--- Front-End/src/app/models/leaves.models.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Leaves } from './leaves.models';
-
-describe('Leaves', () => {
-  it('should create an instance', () => {
-    expect(new Leaves()).toBeTruthy();
-  });
-});
--- Front-End/src/app/models/leaves.models.ts
+++
@@ -1,8 +0,0 @@
-export class Leaves {
-    id?:any;
-    username?:string;
-    des?:string;
-    dt?:string;
-    status?:string;
-    employee_id?:any;
-}
